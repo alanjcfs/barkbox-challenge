6cf90372191c6c173d4771093ebc3c497ed1df32
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2021-11-02 23:38:57 -0700

    Add routes for likes, only creating and destroying

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :dogs
+  resources :likes, only: [:create, :destroy]
   root to: "dogs#index"
 end
